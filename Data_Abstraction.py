0ee30d56923ffcee254d1c7d3284af1eb51639cd
Anngr950 <anngr950@student.liu.se>
2024-04-10 09:58:32 +0200

    Animation, vi tar filips

--- Data_Abstraction.py
+++ Data_Abstraction.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import numpy as np
+import random
+import sys
 
-path = 'Data/EyeTrack-raw.tsv'
+path = 'Advanced_Visual_Data_Analysis\Data\EyeTrack-raw.tsv'
 
 data=pd.read_csv(path,sep='\t')
 
@@ -12,6 +15,7 @@ TIME = data['RecordingTimestamp']
 
 TIME = TIME / 1000
 
+fig, ax = plt.subplots()
 scatter = plt.scatter(X, Y, c=TIME, cmap='viridis')
 cbar = plt.colorbar(scatter, label="Time")
 
@@ -23,4 +27,16 @@ ticks = np.linspace(min(TIME), max(TIME), num=5)
 cbar.set_ticks(ticks)
 cbar.set_ticklabels([str(label) for label in ticks])
 
+
+def update(frame):
+    # for each frame, update the data stored on each artist.
+    x = ticks[:frame]
+    y = ticks[:frame]
+    # update the scatter plot:
+    data = np.stack([x, y]).T
+    scatter.set_offsets(data)
+    return (scatter)
+
+
+ani = animation.FuncAnimation(fig = fig, func=update, frames=40, interval=30)
 plt.show()
