9b22db8f10767700b638009d4945f46c31e01164
filha243 <filha243@student.liu.se>
2024-05-03 14:57:22 +0200

    Arrow graph approach

--- Project - VAST 2020 - JS/chart.js
+++ Project - VAST 2020 - JS/chart.js
@@ -2,11 +2,17 @@ d3.csv("Data/CGCS-Template.csv").then(function (data) {
 
     var nodes = [];
     var links = [];
+    var types = [];
 
     data.forEach(function (d) {
 
         var sourceNode = nodes.find(node => node.id === d.Source);
         var targetNode = nodes.find(node => node.id === d.Target);
+        var Types = nodes.find(node => node.id === d.eType);
+
+        if (d.eType == 5) {
+            return;
+        }
 
         if (!sourceNode) {
             sourceNode = { id: d.Source };
@@ -18,39 +24,104 @@ d3.csv("Data/CGCS-Template.csv").then(function (data) {
             nodes.push(targetNode);
         }
 
+        types.push(Types);
         links.push({ source: sourceNode, target: targetNode });
     });
 
+    const color = d3.scaleOrdinal(types, d3.schemeCategory10);
+
+    var width = 1000;
+    var height = 1000;
+
     var simulation = d3.forceSimulation(nodes)
         .force("link", d3.forceLink(links).id(d => d.id))
-        .force("charge", d3.forceManyBody())
-        .force("center", d3.forceCenter(850, 450));
+        .force("charge", d3.forceManyBody().strength(-500))
+        .force("center", d3.forceCenter(width / 2, height / 2));
 
-    var svg = d3.select("svg");
+    var svg = d3.select("svg")
+        .attr("width", width)
+        .attr("height", height);
 
-    var link = svg.selectAll(".link")
+    svg.append("defs").selectAll("marker")
+        .data(types)
+        .join("marker")
+        .attr("id", d => `arrow-${d}`)
+        .attr("viewBox", "0 -5 10 10")
+        .attr("refX", 15)
+        .attr("refY", -0.5)
+        .attr("markerWidth", 6)
+        .attr("markerHeight", 6)
+        .attr("orient", "auto")
+        .append("path")
+        .attr("fill", color)
+        .attr("d", "M0,-5L10,0L0,5");
+
+    const link = svg.append("g")
+        .attr("fill", "none")
+        .attr("stroke-width", 1.5)
+        .selectAll("path")
         .data(links)
-        .enter().append("line")
-        .attr("class", "link");
+        .join("path")
+        .attr("stroke", d => color(d.type))
+        .attr("marker-end", d => `url(${new URL(`#arrow-${d.type}`, location)})`);
 
-    var node = svg.selectAll(".node")
+    const node = svg.append("g")
+        .attr("fill", "currentColor")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-linejoin", "round")
+        .selectAll("g")
         .data(nodes)
-        .enter().append("circle")
-        .attr("class", "node")
+        .join("g")
+        .call(drag(simulation));
+
+    node.append("circle")
+        .attr("stroke", "black")
+        .attr("stroke-width", 1.5)
         .attr("r", 5);
 
+    node.append("text")
+        .attr("x", 8)
+        .attr("y", "0.31em")
+        .text(d => d.id)
+        .clone(true).lower()
+        .attr("fill", "black")
+        .attr("font-weight", "bold");
 
-    simulation.on("tick", function () {
-        link
-            .attr('x1', d => d.source.x)
-            .attr('y1', d => d.source.y)
-            .attr('x2', d => d.target.x)
-            .attr('y2', d => d.target.y)
-            .attr('stroke-width', 1)
-            .attr('stroke', 'red');
-
-        node
-            .attr("cx", d => d.x)
-            .attr("cy", d => d.y);
+    simulation.on("tick", () => {
+        link.attr("d", linkArc);
+        node.attr("transform", d => `translate(${d.x},${d.y})`);
     });
 });
+
+function linkArc(d) {
+    const r = Math.hypot(d.target.x - d.source.x, d.target.y - d.source.y);
+    return `
+      M${d.source.x},${d.source.y}
+      A${r},${r} 0 0,1 ${d.target.x},${d.target.y}
+    `;
+}
+
+drag = simulation => {
+
+    function dragstarted(event, d) {
+        if (!event.active) simulation.alphaTarget(0.3).restart();
+        d.fx = d.x;
+        d.fy = d.y;
+    }
+
+    function dragged(event, d) {
+        d.fx = event.x;
+        d.fy = event.y;
+    }
+
+    function dragended(event, d) {
+        if (!event.active) simulation.alphaTarget(0);
+        d.fx = null;
+        d.fy = null;
+    }
+
+    return d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended);
+}
