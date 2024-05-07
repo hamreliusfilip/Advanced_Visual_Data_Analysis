33e5cfb52bed8d543efd86271fa156b5aea722e1
filha243 <filha243@student.liu.se>
2024-05-07 15:22:31 +0200

    bug fix från merge

--- Project - VAST 2020 - JS/chart.js
+++ Project - VAST 2020 - JS/chart.js
@@ -122,12 +122,12 @@ function createChart(chartSelector, dataURL, width, height, centerPos) {
         .attr("dy", "0.35em")
         .text(d => `${typeDescription[d]}`);
 
-    simulation.on("tick", () => {
-        link.attr("d", linkArc);
-        node.attr("transform", d => `translate(${d.x},${d.y})`);
+        simulation.on("tick", () => {
+            link.attr("d", linkArc);
+            node.attr("transform", d => `translate(${d.x},${d.y})`);
+        });
     });
-});
-
+    
     function linkArc(d) {
         const r = Math.hypot(d.target.x - d.source.x, d.target.y - d.source.y);
         return `
@@ -135,25 +135,26 @@ function createChart(chartSelector, dataURL, width, height, centerPos) {
           A${r},${r} 0 0,1 ${d.target.x},${d.target.y}
         `;
     }
-
-    function drag(simulation) {
+    
+    drag = simulation => {
+    
         function dragstarted(event, d) {
             if (!event.active) simulation.alphaTarget(0.3).restart();
             d.fx = d.x;
             d.fy = d.y;
         }
-
+    
         function dragged(event, d) {
             d.fx = event.x;
             d.fy = event.y;
         }
-
+    
         function dragended(event, d) {
             if (!event.active) simulation.alphaTarget(0);
             d.fx = null;
             d.fy = null;
         }
-
+    
         return d3.drag()
             .on("start", dragstarted)
             .on("drag", dragged)
